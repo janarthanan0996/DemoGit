dad6f25eb943601b70b276f58704ed95df40d406
Admin <Admin@DESKTOP-PP1VBLD>
2024-01-06 16:08:52 +0530

    commiting Test2 to BranchX

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+		String a="hii";
+		if(a.equals("hii")) {
+			System.out.println("right");
+		}
+		else {
+			System.out.println("wrong");
+		}
+	}
+}
