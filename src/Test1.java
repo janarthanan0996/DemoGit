1f80c67a51cf19bca3fa8762af53ed52be4c76dd
Admin <Admin@DESKTOP-PP1VBLD>
2024-01-06 07:29:57 +0530

    first commit....

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+public static void main(String[] args) {
+	String a="hello";
+	if(a.equals("hello")) {
+		System.out.println("yes");
+	}
+	else {
+		System.out.println("no");
+	}
+}
+}
